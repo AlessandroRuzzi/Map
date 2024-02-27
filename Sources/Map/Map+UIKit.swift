236b44ed95899bb51658e9ac4672f69d554a3a9a
alessandro <alrxruzzi98@gmail.com>
2024-02-27 12:35:01 +0100

    test2

--- Sources/Map/Map+UIKit.swift
+++ Sources/Map/Map+UIKit.swift
@@ -14,30 +14,14 @@ import SwiftUI
 extension Map: UIViewRepresentable {
 
     public func makeUIView(context: Context) -> MKMapView {
-        // let mapView = MKMapView()
-        // mapView.delegate = context.coordinator
-        // updateUIView(mapView, context: context)
-        // return mapView
-
-        print("display lenght", forDisplay.count)
-        ///  creating a map
-        let view = MKMapView()
-        /// connecting delegate with the map
-        view.delegate = context.coordinator
-        view.setRegion(region, animated: false)
-        view.mapType = .standard
-        
-        for points in self.annotationItems {
-            let annotation = LandmarkAnnotation(coordinate: points.coordinate)
-            view.addAnnotation(annotation)
-        }
-        
-
-        return view
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        updateUIView(mapView, context: context)
+        return mapView
     }
 
     public func updateUIView(_ mapView: MKMapView, context: Context) {
-        // context.coordinator.update(mapView, from: self, context: context)
+        context.coordinator.update(mapView, from: self, context: context)
     }
 
 }
